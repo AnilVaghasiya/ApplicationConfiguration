51862c2e77c4ce1ebbe3d59f398807304cbdb233
aakki <aakki@users.noreply.github.com>
2016-08-13 16:24:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* App Module */
+
+var app = angular.module('TestDemo', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'SharedServices', 'fundoo.services' ]);
+
+//utils.initToastr();
+//'angularFileUpload'
